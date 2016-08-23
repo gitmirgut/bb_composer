0cb2bb98aacbf292468503b6151e648d7667fc63
gitmirgut <gitmirgut@users.noreply.github.com>
2016-08-23 17:17:49 +0200

    restructured file organisation for better testing

--- test/test_Adjuster.py
+++ test/test_Adjuster.py
@@ -4,7 +4,7 @@ import numpy as np
 import cv2
 
 c = Composer()
-camera_params_path = 'camera_params_matlab.npz'
+camera_params_path = 'data/camera_params_matlab.npz'
 camera_params = np.load(camera_params_path)
 c.set_camera_params(camera_params)
 test = np.zeros((10, 1, 2), dtype=np.float32)
@@ -12,9 +12,9 @@ test = np.zeros((10, 1, 2), dtype=np.float32)
 # print(test_out[test_out < 0]) TODO problematisch wenn < 0 ?
 print(c.map_coordinates(test))
 img_left_org = cv2.imread(
-    './20160807/Cam_01/Cam_0_20161507130847_631282517.jpg')
+    'data/20160807/Cam_01/Cam_0_20161507130847_631282517.jpg')
 img_right_org = cv2.imread(
-    './20160807/Cam_01/Cam_1_20161507130847_631282517.jpg')
+    'data/20160807/Cam_01/Cam_1_20161507130847_631282517.jpg')
 test = c.compose(img_left_org, img_right_org)
 
 cv2.imwrite("result.png", test)
