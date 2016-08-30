cf9b004ecb115eb31a3c9e7738a6bfe1dcde2371
gitmirgut <gitmirgut@users.noreply.github.com>
2016-08-30 18:47:28 +0200

    add a setup file for distribution'

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='bb_composer',
+    version='0.0.0.dev1',
+
+    description='Composing images from different cam positions',
+    long_description='',
+
+    url='https://github.com/gitmirgut/bb_composer',
+
+    author='gitmirgut',
+    author_email="gitmirgut@users.noreply.github.com",
+    packages=['composer'],
+
+    license='GNU GPLv3',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+
+
+        'Programming Language :: Python :: 3.5'
+    ]
+
+)
